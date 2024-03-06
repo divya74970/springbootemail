6a663f9a99787a0c3e569dfffaf9be489169f2b9
Your Name <you@example.com>
2024-03-06 17:06:49 +0530

    stno[5]email api

---
+++ springbootemailex/src/main/java/com/csi/service/EmailService.java
@@ -0,0 +1,48 @@
+package com.csi.service;
+
+import com.csi.model.EmailModel;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+@Service
+@Slf4j
+public class EmailService {
+
+    @Autowired
+    JavaMailSender javaMailSender;
+
+    public void sendEmail(EmailModel emailModel){
+
+        MimeMessage mimeMessage= javaMailSender.createMimeMessage();
+
+        try {
+            MimeMessageHelper mimeMessageHelper=new MimeMessageHelper(mimeMessage,true);
+
+            mimeMessageHelper.setFrom("karunavg1995@gmail.com");
+
+            mimeMessageHelper.setTo(emailModel.getToEmail());
+            mimeMessageHelper.setCc(emailModel.getCcEmail());
+            mimeMessageHelper.setSubject(emailModel.getCcEmail());
+            mimeMessageHelper.setText(emailModel.getEmailBody());
+
+            FileSystemResource fileSystemResource=new FileSystemResource(emailModel.getEmailAttachment());
+
+            mimeMessageHelper.addAttachment((fileSystemResource.getFilename()),fileSystemResource);
+
+            javaMailSender.send(mimeMessage);
+
+            log.info("Email Send Sucessfully");
+
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
